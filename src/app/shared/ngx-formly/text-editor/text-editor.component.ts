99a13c034bd727e167370ad680b23edde9c71222
m.farhani <mostafaabdfarhani@gmail.com>
2021-03-15 12:04:56 +0330

    + refactor formly widget

--- src/app/shared/ngx-formly/text-editor/text-editor.component.ts
+++ src/app/shared/ngx-formly/text-editor/text-editor.component.ts
@@ -9,6 +9,17 @@ import { TextFormatEnum } from './text-format.enum';
 })
 export class TextEditorComponent extends FieldType implements OnInit {
   @ViewChild('textEditor') textEditor: any;
+  public activeBtn = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
 
   constructor() {
     super();
@@ -21,27 +32,48 @@ export class TextEditorComponent extends FieldType implements OnInit {
   formatText(formatType: number): void {
     switch (formatType) {
       case TextFormatEnum.alignLeft:
+        this.activeBtn[TextFormatEnum.alignLeft] = !this.activeBtn[
+          TextFormatEnum.alignLeft
+        ];
         this.textEditor.nativeElement.style.textAlign = 'left';
         break;
       case TextFormatEnum.alignRight:
+        this.activeBtn[TextFormatEnum.alignRight] = !this.activeBtn[
+          TextFormatEnum.alignRight
+        ];
         this.textEditor.nativeElement.style.textAlign = 'right';
         break;
       case TextFormatEnum.bold:
+        this.activeBtn[TextFormatEnum.bold] = !this.activeBtn[
+          TextFormatEnum.bold
+        ];
         this.textEditor.nativeElement.style.fontWeight = 'bold';
         break;
       case TextFormatEnum.italic:
+        this.activeBtn[TextFormatEnum.italic] = !this.activeBtn[
+          TextFormatEnum.italic
+        ];
         this.textEditor.nativeElement.style.fontStyle = 'italic';
         break;
       case TextFormatEnum.underlined:
+        this.activeBtn[TextFormatEnum.underlined] = !this.activeBtn[
+          TextFormatEnum.underlined
+        ];
         this.textEditor.nativeElement.style.textDecoration = 'underline';
         break;
       case TextFormatEnum.listBulleted:
+        this.activeBtn[TextFormatEnum.listBulleted] = !this.activeBtn[
+          TextFormatEnum.listBulleted
+        ];
         this.textEditor.nativeElement.value = this.textEditor.nativeElement.value.replace(
           /\n/g,
           '\n\u2022  '
         );
         break;
       case TextFormatEnum.listNumbered:
+        this.activeBtn[TextFormatEnum.listNumbered] = !this.activeBtn[
+          TextFormatEnum.listNumbered
+        ];
         for (let i = 0; i <= this.textEditor.nativeElement.value.length; i++) {
           let row = 1;
           if (this.textEditor.nativeElement.value[i] === '\n') {
@@ -51,8 +83,14 @@ export class TextEditorComponent extends FieldType implements OnInit {
         }
         break;
       case TextFormatEnum.size:
+        this.activeBtn[TextFormatEnum.size] = !this.activeBtn[
+          TextFormatEnum.size
+        ];
         break;
       case TextFormatEnum.color:
+        this.activeBtn[TextFormatEnum.color] = !this.activeBtn[
+          TextFormatEnum.color
+        ];
         break;
     }
   }
